e389da9b478a4160a88f05ad3a8a5ea16c50e91d
Justyna <justyna.sienkiewicz93@gmail.com>
2026-01-12 20:50:07 +0100

    add gender; refactor ui

---
+++ SafeCare/Enums/Gender.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SafeCare.Enums
+{
+    public enum Gender
+    {
+        [Display(Name = "Mężczyzna")]
+        Male,
+
+        [Display(Name = "Kobieta")]
+        Female,
+
+        [Display(Name = "Nie chcę podawać")]
+        NotProvided,
+    }
+}
