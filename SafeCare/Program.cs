88ab309320d47e047bee245d9e0146c1832f1ab8
Justyna <justyna.sienkiewicz93@gmail.com>
2026-02-03 22:47:29 +0100

    Integrate Serilog for structured application logging

--- SafeCare/Program.cs
+++ SafeCare/Program.cs
@@ -4,39 +4,62 @@ using MudBlazor.Services;
 using SafeCare.Components;
 using SafeCare.Data;
 using SafeCare.Services;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+        .Build())
+    .CreateLogger();
 
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-builder.Services.AddMudServices(config =>
+try
 {
-    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
-});
+    Log.Information("Starting SafeCare application");
 
-builder.Services.AddDbContextFactory<AppDbContext>();
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IIncidentDefinitionService, IncidentDefinitionService>();
-builder.Services.AddScoped<IDepartmentService, DepartmentService>();
-builder.Services.AddScoped<IIncidentReportService, IncidentReportService>();
-builder.Services.AddScoped<ITimezoneService, TimezoneService>();
+    builder.Host.UseSerilog();
 
-var app = builder.Build();
+    builder.Services.AddRazorComponents()
+        .AddInteractiveServerComponents();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    builder.Services.AddMudServices(config =>
+    {
+        config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
+    });
+
+    builder.Services.AddDbContextFactory<AppDbContext>();
+
+    builder.Services.AddScoped<IIncidentDefinitionService, IncidentDefinitionService>();
+    builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+    builder.Services.AddScoped<IIncidentReportService, IncidentReportService>();
+    builder.Services.AddScoped<ITimezoneService, TimezoneService>();
+
+    var app = builder.Build();
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+    app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+    app.UseHttpsRedirection();
+
+    app.UseAntiforgery();
+
+    app.MapStaticAssets();
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+
+    app.Run();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
-
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
