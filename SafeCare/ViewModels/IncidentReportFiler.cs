199199e05b11d3cb55d1bd51f51ea538a004ddae
Justyna <justyna.sienkiewicz93@gmail.com>
2026-02-07 16:49:31 +0100

    saving query parameters

---
+++ SafeCare/ViewModels/IncidentReportFiler.cs
@@ -0,0 +1,13 @@
+﻿using SafeCare.Enums;
+
+namespace SafeCare.ViewModels
+{
+    public class IncidentReportFiler
+    {
+        public string? FullName { get; set; }
+        public string? PatientFullName { get; set; }
+        public Gender? Gender { get; set; }
+        public string? Department { get; set; }
+        public IEnumerable<IncidentCategory?> Categories { get; set; } = [];
+    }
+}
--- SafeCare/ViewModels/IncidentReportsRequestVm.cs
+++ SafeCare/ViewModels/IncidentReportsRequestVm.cs
@@ -1,26 +1,18 @@
 ﻿using MudBlazor;
-using SafeCare.Enums;
 
 namespace SafeCare.ViewModels
 {
     public class IncidentReportsRequestVm
     {
-        private static SortDefinition<IncidentReportsGridItem> DefaultSort => new(
-            nameof(IncidentReportsGridItem.Id), true, 0, x => x.Id);
+        public const int DefaultPage = 0;
+        public const int DefaultPageSize = 10;
+        public const string DefaultSortBy = nameof(IncidentReportsGridItem.Id);
+        public const bool DefaultSortDescending = true;
 
-
-        public int Page { get; set; } = 0;
-        public int PageSize { get; set; } = 10;
-        public ICollection<SortDefinition<IncidentReportsGridItem>> SortDefinitions { get; set; } = [DefaultSort];
+        public int Page { get; set; } = DefaultPage;
+        public int PageSize { get; set; } = DefaultPageSize;
+        public ICollection<SortDefinition<IncidentReportsGridItem>> SortDefinitions { get; set; } =
+            [new SortDefinition<IncidentReportsGridItem>(DefaultSortBy, DefaultSortDescending, 0, x => x.Id)];
         public IncidentReportFiler Filter { get; set; } = new();
-    }
-
-    public class IncidentReportFiler
-    {
-        public string FullName { get; set; } = string.Empty;
-        public string PatientFullName { get; set; } = string.Empty;
-        public Gender? Gender { get; set; }
-        public string Department { get; set; } = string.Empty;
-        public IEnumerable<IncidentCategory?> Categories { get; set; } = [];
     }
 }
